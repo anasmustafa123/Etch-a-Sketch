d9081f6ed5fc07dbfd429570bb9f364100603167
anas mustafa <anasmuostafa@gmail.com>
2023-02-08 21:44:19 +0200

    adding a random color , modern , default color buttons

--- js/script.js
+++ js/script.js
@@ -1,34 +1,33 @@
+let defaultNumOfmarkers = 16;
+let defaultcolor = "#000000";
+let colorMode = 'default';
 
-//creating the grid with default options 960px width and 960px height
-createGrid("960px","960px")
 //filing the grid with default options 16x16 px 
-fillGrid(16);
+fillGrid(defaultNumOfmarkers);
 
-
-
-
-
-function createGrid(width, height){
-    const container = document.createElement('div');
-    const body = document.querySelector('body');
-    container.classList.add("container");
-    container.style.cssText = `background:  #F8F8FF; border: 1px solid; width: ${width}; height: ${height}; margin: auto;`;
-    body.appendChild(container);
-}
-
-function fillGrid(numOfmakers){
-    const container = document.querySelector('.container');
-    for(let i = 0; i < numOfmakers*numOfmakers; i++){
+//fuction for filling the grid with default options
+function fillGrid(numOfmarkers){
+    const container = document.querySelector('.gridContainer');
+    for(let i = 0; i < numOfmarkers*numOfmarkers; i++){
         const  div = document.createElement("div");
-        let length = 960/numOfmakers + "px" ; //width and heigth of the grid
-        div.style.cssText = `float: left; width: ${length}; height: ${length} `;
+        let width = 600/numOfmarkers + "px" ; //width the grid
+        let height = 500/numOfmarkers + "px"; //height of the grid
+        div.style.cssText = `float: left; width: ${width}; height: ${height} `;
         div.classList.add('marker');
         div.style.color = "red";
         container.appendChild(div);
         div.addEventListener('mouseenter' , (e) => {
             if(clickedGrid){ //changing the color of the square when clicked inside the grid and hovered
-                e.target.classList.add("changeColor");
-                console.log("clicked");
+                if(colorMode === 'default'){
+                    e.target.style.backgroundColor = defaultColor;
+                    e.target.classList.add("changeColor");
+                    console.log("clicked");
+                }else if(colorMode == 'random'){
+                    e.target.style.backgroundColor = getRandomColor();
+                }else if(colorMode == 'random'){
+                    //e.target.style.backgroundColor = getRandomColor();
+                }
+               
             }
         });
         div.addEventListener('mousedown' , (e) => {
@@ -38,6 +37,23 @@ function fillGrid(numOfmakers){
     } 
 }
 
+//setting the clearing button
+const clearGrid = document.getElementById('clearGrid');
+console.log(clearGrid.style);
+clearGrid.addEventListener('click', clearAll);
+
+//setting the input slider
+const slider  = document.querySelector('input');
+const range  = document.querySelector('.range');
+range.textContent = "16x16";
+slider.addEventListener('input', () => {
+    console.log(slider.value);
+    range.textContent = slider.value  +"x"+ slider.value; 
+    removeAll(document.querySelectorAll('.marker'));
+    fillGrid(slider.value);
+});
+
+
 //clearing the grid so the container would cover the  grid
 const div = document.createElement("div");
 div.style.clear = "all;"
@@ -46,7 +62,10 @@ div.style.clear = "all;"
 let clickedGrid = false;
 
 
-const container = document.querySelector('.container');
+const container = document.querySelector('.gridContainer');
+const randomColor = document.querySelector('.random-color');
+const modernColor = document.querySelector('.modern-color');
+const defaultColor = document.querySelector('.default-color');
 
 container.addEventListener('mouseleave', ()  => {
     //enteredGrid = false;
@@ -60,12 +79,38 @@ container.addEventListener('mousedown' , () => {
 
 container.addEventListener("mouseup" , () => {
     clickedGrid = false;
-    console.log("unclick")
+    console.log("unclicked")
 });
-
-function clearGrid(){
-    const markers =querySelectorAll(".marker");
+randomColor.addEventListener('click', () => { //change the color-mode to random
+    colorMode = 'random';
+})
+modernColor.addEventListener('click', () => { 
+    colorMode = 'modern';
+});
+defaultColor.addEventListener('click', () => {
+    colorMode = 'default';
+});
+function clearAll(){
+    const markers =document.querySelectorAll(".marker");
     markers.forEach(marker => {
         marker.classList.remove("changeColor"); 
     });
+}
+
+
+//function to remove all element from a nodeList
+function removeAll(nodeList) {
+    for (let i = 0; i < nodeList.length; i++) {
+        nodeList[i].remove();
+    }
+}
+
+//function to return random color
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
